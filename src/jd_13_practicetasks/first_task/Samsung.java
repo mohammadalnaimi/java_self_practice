d24c54cba803ec3706b48194bbd34bfa4245d3de
mohammadalnaimi <alnaimijo88@gmail.com>
2024-02-12 14:12:56 -0600

    phoneclients added

---
+++ src/jd_13_practicetasks/first_task/Samsung.java
@@ -0,0 +1,24 @@
+package jd_13_practicetasks.first_task;
+
+public class Samsung extends Phone{
+
+
+    public Samsung(String brand, String model, String size, double price, String color) {
+        super(brand, model, size, price, color);
+    }
+
+    @Override
+    public void call(int phoneNumber) {
+        super.call(phoneNumber);
+    }
+
+    @Override
+    public void text(int phoneNumber) {
+        super.text(phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
