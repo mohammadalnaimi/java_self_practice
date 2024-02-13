9610e0a94c124ea213cdff5d0fc4c1ae19ae925a
mohammadalnaimi <alnaimijo88@gmail.com>
2024-02-12 20:57:02 -0600

    classes created

---
+++ src/jd_13_practicetasks/employee_task/Employee.java
@@ -0,0 +1,83 @@
+package jd_13_practicetasks.employee_task;
+
+public class Employee extends Person{
+
+
+    private String employeeId;
+    private String jobTitle;
+    private double salary;
+    private String companyName;
+
+    public Employee(String name, String gender, String employeeId, String jobTitle, double salary, String companyName) {
+        super(name, gender);
+        setEmployeeId(employeeId);
+        setJobTitle(jobTitle);
+        setSalary(salary);
+        setCompanyName(companyName);
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+
+        if (employeeId == null || employeeId.isBlank()  || employeeId.isEmpty()) {
+            throw new RuntimeException("Invalid entry can't be null empty or blank");
+        }
+
+        this.employeeId = employeeId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+
+
+        if (jobTitle == null || jobTitle.isBlank()  || jobTitle.isEmpty()) {
+            throw new RuntimeException("Invalid entry can't be null empty or blank");
+        }
+
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+
+        if (salary <= 0){
+            throw new RuntimeException("Invalid entry can't be zero or less");
+        }
+
+        this.salary = salary;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        if (companyName == null || companyName.isBlank()  || companyName.isEmpty()) {
+            throw new RuntimeException("Invalid entry can't be null empty or blank");
+        }
+        this.companyName = companyName;
+    }
+
+    public void work(){
+        System.out.println("jobTitle "+jobTitle +" Employee name "+getName());
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+"{" +
+                "employeeId='" + employeeId + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                ", companyName='" + companyName + '\'' +
+                '}';
+    }
+}
---
+++ src/jd_13_practicetasks/employee_task/Person.java
@@ -0,0 +1,44 @@
+package jd_13_practicetasks.employee_task;
+
+public class Person {
+
+    private String name;
+    private String gender;
+
+    public Person(String name, String gender){
+        setName(name);
+        setGender(gender);
+
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        if (gender == null || gender.isBlank()  || gender.isEmpty()){
+            throw new RuntimeException("Invalid entry can't be null empty or blank");
+        }
+        this.gender = gender;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+
+        if (gender == null || gender.isBlank()  || gender.isEmpty()) {
+            throw new RuntimeException("Invalid entry can't be null empty or blank");
+        }
+        this.name=name;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+"{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
