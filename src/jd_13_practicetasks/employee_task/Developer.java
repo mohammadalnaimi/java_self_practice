89b78e437e1fb00b2dd7a41de79bc4822ac9bb16
mohammadalnaimi <alnaimijo88@gmail.com>
2024-02-12 21:14:41 -0600

    classes created

---
+++ src/jd_13_practicetasks/employee_task/Developer.java
@@ -0,0 +1,24 @@
+package jd_13_practicetasks.employee_task;
+
+public class Developer extends Employee{
+    private String prgrammingLangauge;
+    public Developer (String name, String gender, String employeeId, String jobTitle, double salary, String companyName,String prgrammingLangauge){
+        super(name,gender,employeeId,jobTitle,salary,companyName);
+    }
+
+    public String getPrgrammingLangauge() {
+        return prgrammingLangauge;
+    }
+
+    public void setPrgrammingLangauge(String prgrammingLangauge) {
+        if (prgrammingLangauge == null || prgrammingLangauge.isBlank()  || prgrammingLangauge.isEmpty()) {
+            throw new RuntimeException("Invalid entry can't be null empty or blank");
+        }
+        this.prgrammingLangauge = prgrammingLangauge;
+    }
+
+    public void work(){
+        System.out.println(getJobTitle()+" "+getName()+" is coding in "+prgrammingLangauge);
+    }
+
+}
